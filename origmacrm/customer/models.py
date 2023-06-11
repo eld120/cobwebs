6d06a952dc2020dc7e137b90ca671d823d978101
Seyamack Afzali <seyamack@gmail.com>
2023-06-10 19:12:33 -0500

    preliminary customer schema

--- origmacrm/customer/models.py
+++ origmacrm/customer/models.py
@@ -1,10 +1,52 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+
+ACTIVE_OPTIONS = (("active", "Active"), ("active", "Inactive"), ("active", "Archived"))
+
+
 class TimeStampModel:
     created_on = models.DateTimeField(auto_now=False, auto_now_add=False)
     updated_on = models.DateTimeField(auto_now=False, auto_now_add=False)
+    start_date = models.DateField(auto_now=False, auto_now_add=False)
+    end_date = models.DateField(auto_now=False, auto_now_add=False)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_(""), on_delete=models.CASCADE)
 
-class Customer:
+class Customer(TimeStampModel):
+
+    INDUSTRY_OPTIONS = (
+        ("agriculture", "Agriculture"),
+        ("arts entertainment", "Arts & Entertainment"),
+        ("construction", "Construction"),
+        ("education", "Education"),
+        ("energy", "Energy"),
+        ("food", "Food & Hospitality"),
+        ("finance", "Finance and Insurance"),
+        ("healthcare", "Healthcare"),
+        ("manufacturing", "Manufacturing"),
+        ("mining", "Mining"),
+        ("other", "Other Services"),
+        ("services", "Professional, Scientific, and Tech Services"),
+        ("real estate", "Real Estate"),
+        ("retail", "Retail"),
+        ("transportation", "Transportation & Logistics"),
+        ("utilities", "Utilities"),
+        ("wholesale", "Wholesale"),
+    )
+
+    dba = models.CharField(max_length=50)
     name = models.CharField(max_length=50)
-    dba  = models.CharField(max_length=50)
+    billing_address = models.ForeignKey("Address", verbose_name=_("Addresses"), on_delete=models.DO_NOTHING)
+    shipping_address = models.ForeignKey("Address", verbose_name=_("Addresses"), on_delete=models.DO_NOTHING) 
+    active = models.CharField(choices=ACTIVE_OPTIONS, max_length=50)
+    customer_type = models.CharField(choices=INDUSTRY_OPTIONS, max_length=100)
+
+class Address(TimeStampModel):
+    address_1 = models.CharField(max_length=100)
+    address_2 = models.CharField(max_length=100)
+    city = models.CharField(_(""), max_length=50)
+    state = models.CharField(_(""), max_length=50)
+    zip_code = models.CharField(_(""), max_length=50)
+    phone = models.CharField(_(""), max_length=50)
+    email = models.CharField(_(""), max_length=50)
