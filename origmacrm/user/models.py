6a5e51ac5d10e0ba28a0e42e116c3e86e5a7aa42
Seyamack Afzali <seyamack@gmail.com>
2023-06-10 19:47:20 -0500

    prelim user app

---
+++ origmacrm/user/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
+
+
+# Create your models here.
+class User(AbstractUser):
+    
+    #: First and last name do not cover name patterns around the globe
+    name = models.CharField(_("Name of User"), blank=True, max_length=255)
+    first_name = None  # type: ignore
+    last_name = None  # type: ignore
+
+    def get_absolute_url(self):
+        """Get url for user's detail view.
+
+        Returns:
+            str: URL for user detail.
+
+        """
+        return reverse("users:detail", kwargs={"username": self.username})
