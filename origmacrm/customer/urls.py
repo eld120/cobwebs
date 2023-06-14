a859c79179e81a86e218031b236b0ac3bb23a106
Seyamack Afzali <seyamack@gmail.com>
2023-06-13 19:48:15 -0500

    adding customer/address create and update views and url routing

--- origmacrm/customer/urls.py
+++ origmacrm/customer/urls.py
@@ -5,7 +5,15 @@ from . import views
 app_name = "customer"
 
 urlpatterns = [
-    path("", views.HomePage.as_view(), name="home"),
     path("dashboard/", views.CustomerListView.as_view(), name="dashboard"),
-    path("<int:pk>/", views.CustomerDetailView.as_view(), name="customer_detail"),
+    path(
+        "customer-update/<int:pk>",
+        views.CustomerUpdateView.as_view(),
+        name="customer_update",
+    ),
+    path(
+        "customer/<int:pk>/", views.CustomerDetailView.as_view(), name="customer_detail"
+    ),
+    path("customer/", views.CustomerCreateView.as_view(), name="customer_create"),
+    path("", views.HomePage.as_view(), name="home"),
 ]
--- origmacrm/customer/views.py
+++ origmacrm/customer/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import DetailView, ListView
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
 from django.views.generic.base import TemplateView
 
 from .models import Customer
@@ -19,3 +19,21 @@ class CustomerListView(ListView):
 class CustomerDetailView(DetailView):
     model = Customer
     template_name = "customer_detail.html"
+
+
+class CustomerCreateView(CreateView):
+    model = Customer
+    template_name = "customer_form.html"
+
+
+class CustomerUpdateView(UpdateView):
+    model = Customer
+    template_name = "customer_form.html"
+
+
+class AddressCreateView(CreateView):
+    pass
+
+
+class AddressUpdateView(UpdateView):
+    pass
