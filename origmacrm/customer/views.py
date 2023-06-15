531dd81c8a813bd8793c80675bb08d02d1024833
Seyamack Afzali <seyamack@gmail.com>
2023-06-14 20:07:52 -0500

    updated views and settings to require authentication

--- origmacrm/customer/views.py
+++ origmacrm/customer/views.py
@@ -1,3 +1,6 @@
+from typing import Any, Dict
+
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
 from django.views.generic.base import TemplateView
@@ -10,30 +13,71 @@ class HomePage(TemplateView):
     template_name = "home.html"
 
 
-class CustomerListView(ListView):
+class CustomerListView(LoginRequiredMixin, ListView):
     model = Customer
     template_name = "dashboard.html"
     # paginate_by = 10
 
 
-class CustomerDetailView(DetailView):
+class CustomerDetailView(LoginRequiredMixin, DetailView):
     model = Customer
     template_name = "customer_detail.html"
 
 
-class CustomerCreateView(CreateView):
+class CustomerCreateView(LoginRequiredMixin, CreateView):
     model = Customer
     template_name = "customer_form.html"
+    fields = (
+        "dba",
+        "name",
+        "billing_address",
+        "shipping_address",
+        "start_date",
+        "end_date",
+        "created_by",
+        "active",
+        "customer_type",
+    )
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["create_customer"] = True
+
+        return context
 
 
-class CustomerUpdateView(UpdateView):
+class CustomerUpdateView(LoginRequiredMixin, UpdateView):
     model = Customer
     template_name = "customer_form.html"
+    fields = (
+        "dba",
+        "name",
+        "billing_address",
+        "shipping_address",
+        "start_date",
+        "end_date",
+        "created_by",
+        "active",
+        "customer_type",
+    )
+
+    # def get_initial(self) -> Dict[str, Any]:
+    #     return {
+    #         "dba": ,
+    #         "name",
+    #         "billing_address",
+    #         "shipping_address",
+    #         "start_date",
+    #         "end_date",
+    #         "created_by",
+    #         "active",
+    #         "customer_type",
+    #     }
 
 
-class AddressCreateView(CreateView):
+class AddressCreateView(LoginRequiredMixin, CreateView):
     pass
 
 
-class AddressUpdateView(UpdateView):
+class AddressUpdateView(LoginRequiredMixin, UpdateView):
     pass
