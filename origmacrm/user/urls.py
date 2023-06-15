841c4a29b1fb38c61ef85aeebacaa1a555f5fbaf
Seyamack Afzali <seyamack@gmail.com>
2023-06-14 19:24:27 -0500

    added user login/logout at a very basic level

--- origmacrm/user/urls.py
+++ origmacrm/user/urls.py
@@ -1,8 +1,22 @@
 from django.urls import path
 
+from . import views
+
 app_name = "user"
 
 urlpatterns = [
+    path("login/", views.UserLoginView.as_view(), name="login"),
+    path("logout/", views.UserLogoutView.as_view(), name="logout"),
+    path(
+        "password-change/",
+        views.UserPasswordChangeView.as_view(),
+        name="password_change",
+    ),
+    path(
+        "password-change/done/",
+        views.UserPasswordChangeDoneView.as_view(),
+        name="password_change_done",
+    ),
     # path("~redirect/", view=user_redirect_view, name="redirect"),
     # path("~update/", view=user_update_view, name="update"),
     # path("<str:username>/", view=user_detail_view, name="detail"),
--- origmacrm/user/views.py
+++ origmacrm/user/views.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordChangeDoneView,
+    PasswordChangeView,
+)
 from django.shortcuts import render
 
+
 # Create your views here.
+class UserLoginView(LoginView):
+    template_name = "user/login.html"
+
+
+class UserLogoutView(LogoutView):
+    pass
+
+
+class UserPasswordChangeView(PasswordChangeView):
+    pass
+
+
+class UserPasswordChangeDoneView(PasswordChangeDoneView):
+    pass
