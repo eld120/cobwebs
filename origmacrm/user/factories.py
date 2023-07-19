69f8d66f1fcfa873640d708ab56ab7eb7cbdca5e
Seyamack Afzali <seyamack@gmail.com>
2023-07-18 20:42:58 -0500

    sufficient (I think/hope) tests for the User app and basic login authorization

--- origmacrm/user/factories.py
+++ origmacrm/user/factories.py
@@ -1,15 +1,12 @@
 import factory
 from django.contrib.auth import get_user_model
 
-from .models import User
-
 
 class UserFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = get_user_model()
-
     username = factory.Sequence(lambda x: f"rando_bot_{x}")
-    first_name = factory.Faker("first_name")
-    last_name = factory.Faker("last_name")
+    name = factory.Faker("name")
     email = factory.Faker("email")
     password = factory.PostGenerationMethodCall("set_password", "password")
+
+    class Meta:
+        model = get_user_model()
--- origmacrm/user/tests/test_views.py
+++ origmacrm/user/tests/test_views.py
@@ -1,10 +1,45 @@
-from django.test import Client, TestCase
+from customer.views import CustomerListView
+from django.test import Client, RequestFactory, TestCase
+
+from ..factories import UserFactory
+from ..views import UserLoginView
 
 
-class TestViews(TestCase):
+class UserTestCase(TestCase):
+    # are all of these requests or responses idk
+
     def setUp(self):
+        self.rf = RequestFactory()
+        self.user = UserFactory(password="testpassword")
         self.client = Client()
 
+    def tearDown(self):
+        del self.user
+
+    def test_home_page(self):
+        response = self.client.get("/")
+        assert response.status_code == 200
+        assert response.template_name[0] == "index.html"
+
+    def test_dashboard(self):
+        request = self.rf.get("/dashboard/")
+        response = CustomerListView.as_view()(request)
+        assert response.status_code == 302
+
+        unauthorized_response = self.client.get("/dashboard/")
+        assert unauthorized_response.status_code == 302
+        login = self.client.login(username=self.user.username, password="testpassword")
+        assert login
+        response = self.client.get("/dashboard/")
+        assert response.status_code == 200
+        assert response.template_name[0] == "dashboard.html"
+
     def test_login(self):
-        response = self.client.get("user/login/")
+        request = self.rf.get("/user/login/")
+        response = UserLoginView.as_view()(request)
+        response.status_code == 200
+        response.template_name[0] == "user/login.html"
+        response = self.client.post(
+            "/user/login", {"username": self.user.username, "password": "testpassword"}
+        )
         assert response.status_code == 200
