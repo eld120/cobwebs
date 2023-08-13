83d933f4d1ce76b132c25224b4f2309b7dfd4244
Seyamack Afzali <seyamack@gmail.com>
2023-08-12 19:19:44 -0500

    updated api1 as created_by/user model was added to the api. necessitating a few updates to the user app

--- origmacrm/api/serializers.py
+++ origmacrm/api/serializers.py
@@ -1,5 +1,5 @@
 from customer.models import Address, Customer
-from rest_framework import serializers
+from rest_framework import serializers, validators
 from user.models import User
 
 
@@ -8,13 +8,19 @@ class CustomerSerializer(serializers.HyperlinkedModelSerializer):
         view_name="api:address-detail",
         lookup_field="uuid",
         many=False,
-        queryset=Customer.objects.all(),
+        queryset=Address.objects.all(),
     )
     shipping_addresses = serializers.HyperlinkedRelatedField(
         view_name="api:address-detail",
         lookup_field="uuid",
         many=True,
-        queryset=Customer.objects.all(),
+        queryset=Address.objects.all(),
+    )
+    created_by = serializers.HyperlinkedRelatedField(
+        view_name="user:user_detail",
+        lookup_field="pk",
+        many=False,
+        queryset=User.objects.all(),
     )
 
     class Meta:
@@ -29,11 +35,27 @@ class CustomerSerializer(serializers.HyperlinkedModelSerializer):
             "end_date",
             "active",
             "customer_type",
+            "created_by",
         )
+        validators = [
+            validators.UniqueTogetherValidator(
+                queryset=Customer.objects.all(),
+                fields=(
+                    "dba",
+                    "name",
+                ),
+            )
+        ]
 
 
 class AddressSerializer(serializers.HyperlinkedModelSerializer):
     primary = serializers.ChoiceField(Address.PRIMARY_CHOICES)
+    created_by = serializers.HyperlinkedRelatedField(
+        view_name="user:user_detail",
+        lookup_field="pk",
+        many=False,
+        queryset=User.objects.all(),
+    )
 
     class Meta:
         model = Address
@@ -48,10 +70,38 @@ class AddressSerializer(serializers.HyperlinkedModelSerializer):
             "zip_code",
             "phone",
             "email",
+            "start_date",
+            "end_date",
+            "active",
+            "created_by",
         )
 
+        class Meta:
+            validators = [
+                validators.UniqueTogetherValidator(
+                    queryset=Address.objects.all(),
+                    fields=(
+                        "address_1",
+                        "address_2",
+                        "city",
+                        "state",
+                        "zip_code",
+                    ),
+                )
+            ]
 
-class UserSerializer(serializers.ModelSerializer):
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = "__all__"
+        fields = (
+            "id",
+            "last_login",
+            "is_superuser",
+            "username",
+            "email",
+            "is_staff",
+            "is_active",
+            "date_joined",
+            "name",
+        )
--- origmacrm/user/views.py
+++ origmacrm/user/views.py
@@ -1,10 +1,15 @@
+from django.conf import settings
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import (
     LoginView,
     LogoutView,
     PasswordChangeDoneView,
     PasswordChangeView,
 )
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.views.generic import DetailView
+
+from .models import User
 
 
 # Create your views here.
@@ -14,6 +19,14 @@ class UserLoginView(LoginView):
 
 class UserLogoutView(LogoutView):
     pass
+
+
+class UserDetailView(LoginRequiredMixin, DetailView):
+    queryset = User.objects.all()
+    template_name = "user/user_detail.html"
+
+    class Meta:
+        model = settings.AUTH_USER_MODEL
 
 
 class UserPasswordChangeView(PasswordChangeView):
